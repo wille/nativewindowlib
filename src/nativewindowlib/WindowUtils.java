a618e801b6ed47f61489adf7ba61171fe0ed6991
redpois0n <red@cmail.nu>
2015-06-17 19:41:58 +0200

    methods converted to native

--- src/nativewindowlib/WindowUtils.java
+++ src/nativewindowlib/WindowUtils.java
@@ -14,27 +14,27 @@ import com.sun.jna.win32.StdCallLibrary;
 public final class WindowUtils {
 
 	/**
-	 * https://stackoverflow.com/questions/3188484/windows-how-to-get-a-list-of-all-visible-windows
+	 * https://stackoverflow.com/questions/3188484/windows-how-to-get-a-list-of-
+	 * all-visible-windows
 	 * 
-	 * https://stackoverflow.com/questions/7521693/converting-c-sharp-to-java-jna-getmodulefilename-from-hwnd
+	 * https://stackoverflow.com/questions/7521693/converting-c-sharp-to-java-
+	 * jna-getmodulefilename-from-hwnd
 	 */
 	public static List<NativeWindow> getWindows() {
 		final List<NativeWindow> inflList = new ArrayList<NativeWindow>();
 		final List<Integer> order = new ArrayList<Integer>();
 
-		int top = User32.INSTANCE.GetTopWindow(0);
+		int top = GetTopWindow(0);
 
 		while (top != 0) {
 			order.add(top);
-			top = User32.INSTANCE.GetWindow(top, User32.GW_HWNDNEXT);
+			top = GetWindow(top, GW_HWNDNEXT);
 		}
 
-		User32.INSTANCE.EnumWindows(new WndEnumProc() {
-			public boolean callback(int hWnd, int lParam) {						
-				inflList.add(new NativeWindow(hWnd));
-				return true;
-			}
-		}, 0);
+		/*
+		 * EnumWindows(new WndEnumProc() { public boolean callback(int hWnd, int
+		 * lParam) { inflList.add(new NativeWindow(hWnd)); return true; } }, 0);
+		 */
 
 		Collections.sort(inflList, new Comparator<NativeWindow>() {
 			public int compare(NativeWindow o1, NativeWindow o2) {
@@ -44,17 +44,19 @@ public final class WindowUtils {
 
 		return inflList;
 	}
-	
+
 	/**
 	 * Returns the current selected window
+	 * 
 	 * @return
 	 */
 	public static NativeWindow getActiveWindow() {
-		return new NativeWindow(User32.INSTANCE.GetForegroundWindow());
+		return new NativeWindow(GetForegroundWindow());
 	}
-	
+
 	/**
 	 * Finds all windows and checks which are visible
+	 * 
 	 * @return all visible windows.
 	 */
 	public static List<NativeWindow> getVisibleWindows() {
@@ -66,154 +68,153 @@ public final class WindowUtils {
 				visible.add(w);
 			}
 		}
-		
+
 		return visible;
 	}
 
 	/**
 	 * Finds all windows and searches for a matching title
-	 * @param title the title to search for
-	 * @return the window with the title that has been searched for, or null if not found.
+	 * 
+	 * @param title
+	 *            the title to search for
+	 * @return the window with the title that has been searched for, or null if
+	 *         not found.
 	 */
 	public static NativeWindow getByTitle(String title) {
-		return new NativeWindow(User32.INSTANCE.FindWindowA(null, title));
+		return new NativeWindow(FindWindowA(null, title));
 	}
-	
+
 	/**
 	 * Prevent instantiation
 	 */
 	private WindowUtils() {
 	}
 
-	public static interface WndEnumProc extends StdCallLibrary.StdCallCallback {
+	public static native boolean callback(int hWnd, int lParam);
 
-		public abstract boolean callback(int hWnd, int lParam);
-	}
+	public static final int GW_HWNDNEXT = 2;
 
-	public static interface User32 extends StdCallLibrary {
+	/**
+	 * Enumerate all native windows
+	 * 
+	 * @param wndenumproc
+	 * @param lParam
+	 * @return
+	 */
+	public static native boolean EnumWindows(int wndenumproc, int lParam); // TODO
 
-		public static final User32 INSTANCE = (User32) Native.loadLibrary("user32", User32.class);
+	public static native int FindWindowA(String lpClassName, String lpWindowName);
 
-		public static final int GW_HWNDNEXT = 2;
+	/**
+	 * Is window visible (only show windows that is)
+	 * 
+	 * @param hWnd
+	 * @return
+	 */
+	public static native boolean IsWindowVisible(int hWnd);
 
-		/**
-		 * Enumerate all native windows
-		 * @param wndenumproc
-		 * @param lParam
-		 * @return
-		 */
-		public abstract boolean EnumWindows(WndEnumProc wndenumproc, int lParam);
-		
-		public abstract int FindWindowA(String lpClassName, String lpWindowName);
+	/**
+	 * Gets window position
+	 * 
+	 * @param hWnd
+	 * @param r
+	 * @return
+	 */
+	public static native int GetWindowRect(int hWnd, NativeRectangle r);
 
-		/**
-		 * Is window visible (only show windows that is)
-		 * @param hWnd
-		 * @return
-		 */
-		public abstract boolean IsWindowVisible(int hWnd);
+	/**
+	 * Moves window position
+	 * 
+	 * @param hWnd
+	 * @param X
+	 * @param Y
+	 * @param nWidth
+	 * @param nHeight
+	 * @param bRepaint
+	 * @return
+	 */
+	public static native boolean MoveWindow(int hWnd, int X, int Y, int nWidth, int nHeight, boolean bRepaint);
 
-		/**
-		 * Gets window position
-		 * @param hWnd
-		 * @param r
-		 * @return
-		 */
-		public abstract int GetWindowRect(int hWnd, NativeRectangle r);
+	/**
+	 * Gets window title
+	 * 
+	 * @param hWnd
+	 * @param buffer
+	 * @param buflen
+	 */
+	public static native void GetWindowTextA(int hWnd, byte[] buffer, int buflen);
 
-		/**
-		 * Moves window position
-		 * @param hWnd
-		 * @param X
-		 * @param Y
-		 * @param nWidth
-		 * @param nHeight
-		 * @param bRepaint
-		 * @return
-		 */
-		public abstract boolean MoveWindow(int hWnd, int X, int Y, int nWidth, int nHeight, boolean bRepaint);
+	/**
+	 * Sets window title
+	 * 
+	 * @param hWnd
+	 * @param text
+	 * @return
+	 */
+	public static native boolean SetWindowTextA(int hWnd, String text);
 
-		/**
-		 * Gets window title
-		 * @param hWnd
-		 * @param buffer
-		 * @param buflen
-		 */
-		public abstract void GetWindowTextA(int hWnd, byte[] buffer, int buflen);
-		
-		/**
-		 * Sets window title
-		 * @param hWnd
-		 * @param text
-		 * @return
-		 */
-		public abstract boolean SetWindowTextA(int hWnd, String text);
+	public static native int GetWindowThreadProcessId(int hWnd);
 
-		public abstract int GetWindowThreadProcessId(int hWnd);
+	/**
+	 * Gets window on top
+	 * 
+	 * @param hWnd
+	 * @return
+	 */
+	public static native int GetTopWindow(int hWnd);
 
-		/**
-		 * Gets window on top
-		 * @param hWnd
-		 * @return
-		 */
-		public abstract int GetTopWindow(int hWnd);
+	public static native int GetWindow(int hWnd, int flag);
 
-		public abstract int GetWindow(int hWnd, int flag);
+	/**
+	 * Set focus on window
+	 * 
+	 * @param hWnd
+	 * @return
+	 */
+	public static native boolean SetForegroundWindow(int hWnd);
 
-		/**
-		 * Set focus on window
-		 * @param hWnd
-		 * @return
-		 */
-		public abstract boolean SetForegroundWindow(int hWnd);
+	public static native int GetWindowThreadProcessId(int hWnd, IntByReference pid);
 
-		public abstract int GetWindowThreadProcessId(int hWnd, IntByReference pid);
-		
-		/**
-		 * Minimizes the window
-		 * @param hWnd
-		 * @return
-		 */
-		public abstract boolean CloseWindow(int hWnd);
-		
-		/**
-		 * Closes the window
-		 * @param hWnd
-		 * @return
-		 */
-		public abstract boolean DestroyWindow(int hWnd);
+	/**
+	 * Minimizes the window
+	 * 
+	 * @param hWnd
+	 * @return
+	 */
+	public static native boolean CloseWindow(int hWnd);
 
-		/**
-		 * Changes state of the window
-		 * @param hWnd
-		 * @param nCmdShow
-		 * @return
-		 */
-		public abstract boolean ShowWindow(int hWnd, int nCmdShow);
-		
-		/**
-		 * Returns the current Window HWND
-		 * @return
-		 */
-		public abstract int GetForegroundWindow();
-	}
+	/**
+	 * Closes the window
+	 * 
+	 * @param hWnd
+	 * @return
+	 */
+	public static native boolean DestroyWindow(int hWnd);
+
+	/**
+	 * Changes state of the window
+	 * 
+	 * @param hWnd
+	 * @param nCmdShow
+	 * @return
+	 */
+	public static native boolean ShowWindow(int hWnd, int nCmdShow);
+
+	/**
+	 * Returns the current Window HWND
+	 * 
+	 * @return
+	 */
+	public static native int GetForegroundWindow();
 
 	public static final int PROCESS_QUERY_INFORMATION = 0x0400;
 
-	public interface Kernel32 extends StdCallLibrary {
-		Kernel32 INSTANCE = (Kernel32) Native.loadLibrary("kernel32", Kernel32.class);
+	public static native int /*pointer*/ OpenProcess(int dwDesiredAccess, boolean bInheritHandle, int dwProcessId);
 
-		public Pointer OpenProcess(int dwDesiredAccess, boolean bInheritHandle, int dwProcessId);
+	public static native int GetTickCount();
 
-		public int GetTickCount();
-	};
-
-	public interface PsAPI extends StdCallLibrary {
-		PsAPI INSTANCE = (PsAPI) Native.loadLibrary("psapi", PsAPI.class);
-
-		int GetModuleFileNameExA(Pointer process, Pointer hModule, byte[] lpString, int nMaxCount);
-
-	};
+	// PSAPI
+	public static native int GetModuleFileNameExA(int /*pointer*/ process, int /*pointer*/ hModule, byte[] lpString, int nMaxCount);
 
 	public static class NativeRectangle extends Structure {
 
