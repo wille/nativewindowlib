bc333d2385ba94ba2c99b47775c6ac1ec81acc1b
redpois0n <red@cmail.nu>
2015-06-19 12:09:16 +0200

    Begin testing for X11

--- natives/x11/WindowUtils.c
+++ natives/x11/WindowUtils.c
@@ -30,8 +30,65 @@ JNIEXPORT jboolean JNICALL Java_nativewindowlib_WindowUtils_setWindowText(JNIEnv
 
 }
 
-JNIEXPORT void JNICALL Java_nativewindowlib_WindowUtils_enumWindows(JNIEnv * env, jclass z) {
 
+Window *winlist (Display *disp, unsigned long *len) {
+	Atom prop = XInternAtom(disp,"_NET_CLIENT_LIST",False), type;
+	int form;
+	unsigned long remain;
+	unsigned char *list;
+
+	errno = 0;
+	if (XGetWindowProperty(disp,XDefaultRootWindow(disp),prop,0,1024,False,XA_WINDOW,
+					&type,&form,len,&remain,&list) != Success) {
+		perror("winlist() -- GetWinProp");
+		return 0;
+	}
+
+	return (Window*)list;
+}
+
+char *winame (Display *disp, Window win) {
+	Atom prop = XInternAtom(disp,"WM_NAME",False), type;
+	int form;
+	unsigned long remain, len;
+	unsigned char *list;
+
+	errno = 0;
+	if (XGetWindowProperty(disp,win,prop,0,1024,False,XA_STRING,
+					&type,&form,&len,&remain,&list) != Success) {
+		perror("winlist() -- GetWinProp");
+		return NULL;
+	}
+
+	return (char*)list;
+}
+
+JNIEXPORT void JNICALL Java_nativewindowlib_WindowUtils_enumWindows(JNIEnv * env, jclass z) {
+	int i;
+	unsigned long len;
+	Display *disp = XOpenDisplay(NULL);
+	Window *list;
+	char *name;
+
+	if (!disp) {
+		puts("no display!");
+		return -1;
+	}
+
+	list = (Window*)winlist(disp,&len);
+
+	Window root = list[0];
+
+	for (i=1;i<(int)len;i++) {
+		Window window = list[i];
+		callback(env, (int) window);
+	}
+
+	XFree(list);
+
+	XCloseDisplay(disp);
+
+	callback(env, nativewindowlib_WindowUtils_CALLBACK_COMPLETED);
 }
 
 JNIEXPORT jstring JNICALL Java_nativewindowlib_WindowUtils_getProcessFromWindow(JNIEnv * env, jclass z, jint handle) {
